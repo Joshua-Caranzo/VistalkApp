ebf31afb5fc9ca0b9d577f47ba53eec032ad427d
drich1109 <aldrichbatislaon@gmail.com>
2024-11-24 21:02:55 +0800

    fix bug

--- VistalkWebApp/src/routes/donate/+page.svelte
+++ VistalkWebApp/src/routes/donate/+page.svelte
@@ -200,15 +200,14 @@
 
       <div class="group">
         <svg
-        width="1.2rem" height="1.2rem"
-          class="transition duration-300 ease-in-out fill-black group-hover:fill-[#6addd0]"
+        class="transition duration-300 ease-in-out fill-black group-hover:fill-[#6addd0]"
           xmlns="http://www.w3.org/2000/svg"
+          width="1.2em"
+          height="1.2em"
           viewBox="0 0 24 24"
-        >
-          <path
-            d="M16.88 2.88a1.25 1.25 0 0 0-1.77 0L6.7 11.29a.996.996 0 0 0 0 1.41l8.41 8.41c.49.49 1.28.49 1.77 0s.49-1.28 0-1.77L9.54 12l7.35-7.35c.48-.49.48-1.28-.01-1.77"
-          />
-        </svg>
+          ><path
+            d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z"
+          /></svg>
       </div>
       <span
         class="text-black text-lg transition duration-300 ease-in-out group-hover:text-transparent bg-clip-text bg-gradient-to-r from-[#6addd0] to-[#f7c188]"
